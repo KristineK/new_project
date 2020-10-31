0344542aa3d29a137d4e780d9089335eaef88af6
irigri-riga <irina.grisanova@protonmail.com>
2020-10-31 19:47:23 +0200

    Cucumber tasks

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,9 +3,13 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
 
 public class Task1 {
     WebDriver driver;
@@ -28,20 +32,40 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+        WebElement numberField = driver.findElement(By.id("numb"));
+        numberField.clear();
+        numberField.sendKeys("testtext");
+        driver.findElement(By.className("w3-btn")).click();
+        String expectedR = "Please enter a number";
+        String actualR = driver.findElement(By.xpath("//*[@id='ch1_error']")).getText();
+        assertEquals(expectedR, actualR);
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+        WebElement numberField = driver.findElement(By.id("numb"));
+        numberField.clear();
+        numberField.sendKeys("5");
+        driver.findElement(By.className("w3-btn")).click();
+        String expectedS = "Number is too small";
+        String actualS = driver.findElement(By.id("ch1_error")).getText();
+        assertEquals(expectedS, actualS);
     }
 
     @Test
     public void errorOnNumberTooBig() {
-
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        WebElement numberField = driver.findElement(By.id("numb"));
+        numberField.clear();
+        numberField.sendKeys("222");
+        driver.findElement(By.className("w3-btn")).click();
+        String expectedS = "Number is too big";
+        String actualS = driver.findElement(By.id("ch1_error")).getText();
+        assertEquals(expectedS, actualS);
     }
 
     @Test
@@ -49,6 +73,12 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+        WebElement numberField = driver.findElement(By.id("numb"));
+        numberField.clear();
+        numberField.sendKeys("81");
+        driver.findElement(By.className("w3-btn")).click();
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Square root of 81 is 9.00", alert.getText());
     }
 
     @Test
@@ -56,5 +86,11 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
+        WebElement numberField = driver.findElement(By.id("numb"));
+        numberField.clear();
+        numberField.sendKeys("77");
+        driver.findElement(By.className("w3-btn")).click();
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Square root of 77 is 8.77", alert.getText());
     }
 }
--- src/selenium/tasks/Task2.java
+++ src/selenium/tasks/Task2.java
@@ -3,8 +3,16 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
 
 public class Task2 {
     WebDriver driver;
@@ -14,7 +22,7 @@ public class Task2 {
         String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://kristinek.github.io/sitetasks/provide_feedback");
+        driver.get("https://kristinek.github.io/site/tasks/provide_feedback");
     }
 
     @After
@@ -26,54 +34,208 @@ public class Task2 {
     public void initialFeedbackPage() throws Exception {
 //         TODO:
 //         check that all field are empty and no tick are clicked
+        WebElement nameField = driver.findElement(By.id("fb_name")); //name
+        nameField.clear();
+        WebElement ageField = driver.findElement(By.id("fb_age"));  //age
+        ageField.clear();
+
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector(".w3-check[type='checkbox']")); //language
+        for (WebElement checkBox : checkBoxes) {
+            assertFalse(checkBox.isSelected());
+        }
+
+        WebElement commentField = driver.findElement(By.xpath("//*[@id='fb_form']/form/div[6]/textarea")); //comment
+        commentField.clear();
+
 //         "Don't know" is selected in "Genre"
+        WebElement radioButton3 = driver.findElement(By.xpath("//*[@id='fb_form']/form/div[4]/input[3]"));
+        assertTrue(radioButton3.isSelected());
+
 //         "Choose your option" in "How do you like us?"
+        Select dropdown = new Select(driver.findElement(By.id("like_us")));
+        dropdown.selectByVisibleText("Why me?");
+        assertEquals("Why me?", dropdown.getFirstSelectedOption().getText());
+
 //         check that the button send is blue with white letters
+        WebElement sendButton = driver.findElement(By.xpath("//*[@id='fb_form']/form/button"));
+        assertEquals("rgba(33, 150, 243, 1)", sendButton.getCssValue("background-color"));
+        assertEquals("rgb(255, 255, 255)", sendButton.getCssValue("text-decoration-color"));
     }
 
     @Test
     public void emptyFeedbackPage() throws Exception {
 //         TODO:
+//         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //         click "Send" without entering any data
-//         check fields are empty or null
+        driver.findElement(By.xpath("//*[@id='fb_form']/form/button")).click();
+
+//        check fields are empty or null
+        assertEquals("", driver.findElement(By.id("name")).getText());
+        assertEquals("", driver.findElement(By.id("age")).getText());
+        assertEquals("", driver.findElement(By.id("language")).getText());
+        assertEquals("null", driver.findElement(By.id("gender")).getText());
+        assertEquals("null", driver.findElement(By.id("option")).getText());
+        assertEquals("", driver.findElement(By.id("comment")).getText());
+
 //         check button colors
 //         (green with white letter and red with white letters)
+        WebElement yesButton = driver.findElement(By.cssSelector("button.w3-btn.w3-green.w3-xlarge"));
+        assertEquals("rgba(76, 175, 80, 1)", yesButton.getCssValue("background-color"));
+        assertEquals("rgb(255, 255, 255)", yesButton.getCssValue("text-decoration-color"));
+
+        WebElement noButton = driver.findElement(By.cssSelector("button.w3-btn.w3-red.w3-xlarge"));
+        assertEquals("rgba(244, 67, 54, 1)", noButton.getCssValue("background-color"));
+        assertEquals("rgb(255, 255, 255)", noButton.getCssValue("text-decoration-color"));
     }
 
     @Test
     public void notEmptyFeedbackPage() throws Exception {
 //         TODO:
-//         fill the whole form, click "Send"
+//         fill the whole form,
+        WebElement name = driver.findElement(By.id("fb_name"));
+        name.sendKeys("Irina");
+        WebElement age = driver.findElement(By.id("fb_age"));
+        Thread.sleep(1000);
+        age.sendKeys("36");
+        Thread.sleep(1000);
+        WebElement engLanguage = driver.findElement(By.cssSelector(".w3-check[type='checkbox'][value='English']"));
+        engLanguage.click();
+        Thread.sleep(1000);
+        WebElement female = driver.findElement(By.cssSelector(".w3-radio[type='radio'][value=female]"));
+        female.click();
+        Thread.sleep(1000);
+        Select dropdown = new Select(driver.findElement(By.id("like_us")));
+        dropdown.selectByVisibleText("Good");
+        Thread.sleep(1000);
+        WebElement comment = driver.findElement(By.cssSelector("#fb_form > form > div:nth-child(6) > textarea"));
+        comment.sendKeys("this is a test");
+        Thread.sleep(1000);
+
+//          click "Send"
+        driver.findElement(By.xpath("//*[@id='fb_form']/form/button")).click();
+
 //         check fields are filled correctly
+        assertEquals("Irina", driver.findElement(By.id("name")).getText());
+        assertEquals("36", driver.findElement(By.id("age")).getText());
+        assertEquals("English", driver.findElement(By.id("language")).getText());
+        assertEquals("female", driver.findElement(By.id("gender")).getText());
+        assertEquals("Good", driver.findElement(By.id("option")).getText());
+        assertEquals("this is a test", driver.findElement(By.id("comment")).getText());
+        Thread.sleep(3000);
+
 //         check button colors
 //         (green with white letter and red with white letters)
+        WebElement yesButton = driver.findElement(By.cssSelector("button.w3-btn.w3-green.w3-xlarge"));
+        assertEquals("rgba(76, 175, 80, 1)", yesButton.getCssValue("background-color"));
+        assertEquals("rgb(255, 255, 255)", yesButton.getCssValue("text-decoration-color"));
+
+        WebElement noButton = driver.findElement(By.cssSelector("button.w3-btn.w3-red.w3-xlarge"));
+        assertEquals("rgba(244, 67, 54, 1)", noButton.getCssValue("background-color"));
+        assertEquals("rgb(255, 255, 255)", noButton.getCssValue("text-decoration-color"));
+
     }
 
     @Test
     public void yesOnWithNameFeedbackPage() throws Exception {
 //         TODO:
 //         enter only name
+        WebElement name = driver.findElement(By.id("fb_name"));
+        name.sendKeys("Name");
+        Thread.sleep(2000);
+
 //         click "Send"
+        driver.findElement(By.xpath("//*[@id='fb_form']/form/button")).click();
+        Thread.sleep(2000);
+
 //         click "Yes"
+        driver.findElement(By.cssSelector("button.w3-btn.w3-green.w3-xlarge")).click();
+
 //         check message text: "Thank you, NAME, for your feedback!"
+        String yesMessage = ("Thank you, Name, for your feedback!");
+        assertEquals(yesMessage, driver.findElement(By.id("message")).getText());
+        Thread.sleep(1000);
+
 //         color of text is white with green on the background
+        WebElement messageField = driver.findElement(By.cssSelector("div.w3-panel.w3-green"));
+        assertEquals("rgba(76, 175, 80, 1)", messageField.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)", messageField.getCssValue("color"));
+
     }
 
     @Test
     public void yesOnWithoutNameFeedbackPage() throws Exception {
 //         TODO:
-//         click "Send" (without entering anything
+//         click "Send" (without entering anything)
+        driver.findElement(By.xpath("//*[@id='fb_form']/form/button")).click();
+
 //         click "Yes"
+        driver.findElement(By.cssSelector("button.w3-btn.w3-green.w3-xlarge")).click();
+
 //         check message text: "Thank you for your feedback!"
+        String feedbackMessage = ("Thank you for your feedback!");
+        assertEquals(feedbackMessage, driver.findElement(By.id("message")).getText());
+        Thread.sleep(1000);
+
 //         color of text is white with green on the background
+        WebElement messageField = driver.findElement(By.cssSelector("div.w3-panel.w3-green"));
+        assertEquals("rgba(76, 175, 80, 1)", messageField.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)", messageField.getCssValue("color"));
     }
 
     @Test
     public void noOnFeedbackPage() throws Exception {
 //         TODO:
 //         fill the whole form
+        WebElement name = driver.findElement(By.id("fb_name"));
+        name.sendKeys("Irina");
+        WebElement age = driver.findElement(By.id("fb_age"));
+        Thread.sleep(500);
+        age.sendKeys("36");
+        Thread.sleep(500);
+        WebElement engLanguage = driver.findElement(By.cssSelector(".w3-check[type='checkbox'][value='English']"));
+        engLanguage.click();
+        Thread.sleep(500);
+        WebElement female = driver.findElement(By.cssSelector(".w3-radio[type='radio'][value=female]"));
+        female.click();
+        Thread.sleep(500);
+        Select dropdown = new Select(driver.findElement(By.id("like_us")));
+        dropdown.selectByVisibleText("Good");
+        Thread.sleep(500);
+        WebElement comment = driver.findElement(By.cssSelector("#fb_form > form > div:nth-child(6) > textarea"));
+        comment.sendKeys("this is a test");
+        Thread.sleep(500);
+
 //         click "Send"
+        driver.findElement(By.xpath("//*[@id='fb_form']/form/button")).click();
+        Thread.sleep(1000);
+
 //         click "No"
+        driver.findElement(By.cssSelector("button.w3-btn.w3-red.w3-xlarge")).click();
+        Thread.sleep(1000);
+
 //         check fields are filled correctly
+        WebElement nameField = driver.findElement(By.id("fb_name"));
+        String userName = "Irina";
+        assertEquals(nameField.getText(), ""); // checking that getText is empty
+        assertEquals(nameField.getAttribute("value"), userName);
+
+        WebElement ageField = driver.findElement(By.id("fb_age"));
+        String userAge = "36";
+        assertEquals(ageField.getText(), ""); // checking that getText is empty
+        assertEquals(ageField.getAttribute("value"), userAge);
+        Thread.sleep(1000);
+
+        WebElement engOption = driver.findElement(By.cssSelector(".w3-check[type='checkbox'][value='English']"));
+        assertTrue(engOption.isSelected());
+
+        WebElement femOption = driver.findElement(By.cssSelector(".w3-radio[type='radio'][value=female]"));
+        assertTrue(femOption.isSelected());
+
+        Select newDropdown = new Select(driver.findElement(By.id("like_us")));
+        assertEquals("Good", newDropdown.getFirstSelectedOption().getText());
+
+        WebElement commentField = driver.findElement(By.cssSelector("#fb_form > form > div:nth-child(6) > textarea"));
+        assertEquals(commentField.getText(), ""); // checking that getText is empty
+        assertEquals(commentField.getAttribute("value"), "this is a test");
     }
 }
