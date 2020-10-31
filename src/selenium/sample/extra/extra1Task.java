a21682c803f4a25c7a63a9f8977069a0c3ac2c30
EsportsKing <divpadsmitais@gmail.com>
2020-10-31 21:30:43 +0200

    Finished bonus task 1

--- src/selenium/sample/extra/extra1Task.java
+++ src/selenium/sample/extra/extra1Task.java
@@ -3,8 +3,11 @@ package selenium.sample.extra;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.*;
 
 public class extra1Task {
     WebDriver driver;
@@ -32,31 +35,62 @@ public class extra1Task {
 
     @Test
     public void navigateBack() throws Exception {
-//        TODO
 //        open page with url "https://kristinek.github.io/site/examples/po"
+        driver.get("https://kristinek.github.io/site/examples/po");
 //        click "More > " for the top left element
+        WebElement moreButton = driver.findElement(By.xpath("//body/div[3]/div[2]/div[1]/div[1]/p[1]/a[1]"));
+        moreButton.click();
+        Thread.sleep(1000);
 //        check that the url now "https://kristinek.github.io/site/examples/po1"
+        assertEquals("https://kristinek.github.io/site/examples/po1", driver.getCurrentUrl());
+
 //        using driver navigation go back to "https://kristinek.github.io/site/examples/po"
+        driver.navigate().back();
+        Thread.sleep(1000);
+
 //        check that the page now is "https://kristinek.github.io/site/examples/po"
+        assertEquals("https://kristinek.github.io/site/examples/po", driver.getCurrentUrl());
+
     }
 
     @Test
     public void navigateForward() throws Exception {
-//        TODO
 //        open page with url "https://kristinek.github.io/site/examples/po"
+
+        driver.get("https://kristinek.github.io/site/examples/po");
 //        click "More > " for the top left element
+        WebElement moreButton = driver.findElement(By.xpath("//body/div[3]/div[2]/div[1]/div[1]/p[1]/a[1]"));
+        moreButton.click();
+        Thread.sleep(1000);
+
 //        using driver navigation go back to "https://kristinek.github.io/site/examples/po"
+        driver.navigate().back();
+        Thread.sleep(1000);
+
 //        using driver navigation go forward to "https://kristinek.github.io/site/examples/po1"
+        driver.navigate().forward();
+        Thread.sleep(1000);
+
 //        check that the page now is "https://kristinek.github.io/site/examples/po1"
+        assertEquals("https://kristinek.github.io/site/examples/po1", driver.getCurrentUrl());
     }
 
     @Test
     public void refresh() throws Exception {
-//        TODO
 //        open page "https://kristinek.github.io/site/examples/act"
+        driver.get("https://kristinek.github.io/site/examples/actions");
+
 //        click on "Show" button in 'Button' section
+        WebElement showButton = driver.findElement(By.xpath("//button[@id='show_text']"));
+        showButton.click();
+
 //        check that text "I am here!" is seen
+        assertTrue(driver.findElement(By.cssSelector("#show_me")).isDisplayed());
+
 //        refresh page
+        driver.navigate().refresh();
+
 //        check that text "I am here!" is not seen
+        assertFalse(driver.findElement(By.cssSelector("#show_me")).isDisplayed());
     }
 }
